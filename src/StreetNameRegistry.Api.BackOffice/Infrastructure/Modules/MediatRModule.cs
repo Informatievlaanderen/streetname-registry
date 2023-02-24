d938f7c3ce6c287396bb0a5cf2ec246ee1e76e8f
Luk Vermeulen <luk.vermeulen@gmail.com>
2023-02-24 17:01:59 +0100

    fix: remove ServiceFactory delegate

--- src/StreetNameRegistry.Api.BackOffice/Infrastructure/Modules/MediatRModule.cs
+++ src/StreetNameRegistry.Api.BackOffice/Infrastructure/Modules/MediatRModule.cs
@@ -15,13 +15,6 @@ namespace StreetNameRegistry.Api.BackOffice.Infrastructure.Modules
                 .As<IMediator>()
                 .InstancePerLifetimeScope();
 
-            // request & notification handlers
-            builder.Register<ServiceFactory>(context =>
-            {
-                var ctx = context.Resolve<IComponentContext>();
-                return type => ctx.Resolve(type);
-            });
-
             builder.RegisterAssemblyTypes(typeof(ProposeStreetNameHandler).GetTypeInfo().Assembly).AsImplementedInterfaces();
         }
     }
