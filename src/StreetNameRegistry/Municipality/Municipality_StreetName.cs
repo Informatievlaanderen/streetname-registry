03674797799b8fff26ec5921eb90714aa2c07351
pgallik <patrickgallik@gmail.com>
2023-03-03 11:39:18 +0100

    controller + sqshandler

--- src/StreetNameRegistry/Municipality/Municipality_StreetName.cs
+++ src/StreetNameRegistry/Municipality/Municipality_StreetName.cs
@@ -163,6 +163,11 @@ namespace StreetNameRegistry.Municipality
         {
             var streetName = StreetNames.FindByPersistentLocalId(persistentLocalId);
 
+            if (streetName is null)
+            {
+                throw new StreetNameIsNotFoundException(persistentLocalId);
+            }
+
             if (streetName.IsRemoved)
             {
                 return;
