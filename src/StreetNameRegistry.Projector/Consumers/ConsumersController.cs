027923a5813f02aa9e79e1ea9247fb62072d47d2
Arne Dumarey <dumarey.arne@gmail.com>
2023-06-04 16:55:15 +0200

    feat: add consumer status endpoint in projector GAWR-4879

---
+++ src/StreetNameRegistry.Projector/Consumers/ConsumersController.cs
@@ -0,0 +1,40 @@
+﻿namespace StreetNameRegistry.Projector.Consumers
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Be.Vlaanderen.Basisregisters.Api;
+    using Be.Vlaanderen.Basisregisters.MessageHandling.Kafka.Consumer;
+    using Consumer;
+    using Dapper;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Data.SqlClient;
+    using Microsoft.Extensions.Configuration;
+    using StreetNameRegistry.Infrastructure;
+
+    [ApiVersion("1.0")]
+    [ApiRoute("consumers")]
+    public class ConsumersController : ApiController
+    {
+        private const string? ConsumerConnectionStringKey = "Consumer";
+
+        [HttpGet]
+        public async Task<IActionResult> Get(
+            [FromServices] IConfiguration configuration,
+            CancellationToken cancellationToken = default)
+        {
+            await using var sqlConnection = new SqlConnection(configuration.GetConnectionString(ConsumerConnectionStringKey));
+            var result =
+                await sqlConnection.QueryFirstAsync<ProcessedMessage>(
+                    $"SELECT * FROM [{Schema.Consumer}].[{IdempotentConsumerContext.ProcessedMessageTable}] ORDER BY [{nameof(ProcessedMessage.DateProcessed)}] DESC");
+
+            return Ok(new []
+            {
+                new
+                {
+                    Name = "Consumer van gemeente",
+                    LastProcessedMessage = result
+                }
+            });
+        }
+    }
+}
