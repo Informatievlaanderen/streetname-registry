c839968b043282655e54159c3c28e94e334f2aad
Arne Dumarey <dumarey.arne@gmail.com>
2023-09-01 08:47:03 +0200

    perf: recreate wms index to add include niscode

--- src/StreetNameRegistry.Projections.Wms/Migrations/20230901064608_RecreateWmsIndexRemoved.Designer.cs
+++ src/StreetNameRegistry.Projections.Wms/Migrations/20230901064608_RecreateWmsIndexRemoved.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StreetNameRegistry.Projections.Wms;
 
@@ -11,9 +12,10 @@ using StreetNameRegistry.Projections.Wms;
 namespace StreetNameRegistry.Projections.Wms.Migrations
 {
     [DbContext(typeof(WmsContext))]
-    partial class WmsContextModelSnapshot : ModelSnapshot
+    [Migration("20230901064608_RecreateWmsIndexRemoved")]
+    partial class RecreateWmsIndexRemoved
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/StreetNameRegistry.Projections.Wms/Migrations/20230901064608_RecreateWmsIndexRemoved.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StreetNameRegistry.Projections.Wms.Migrations
+{
+    public partial class RecreateWmsIndexRemoved : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_StreetNameHelperV2_Removed",
+                schema: "wms.streetname",
+                table: "StreetNameHelperV2");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StreetNameHelperV2_Removed",
+                schema: "wms.streetname",
+                table: "StreetNameHelperV2",
+                column: "Removed")
+                .Annotation("SqlServer:Include", new[] { "NisCode" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_StreetNameHelperV2_Removed",
+                schema: "wms.streetname",
+                table: "StreetNameHelperV2");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StreetNameHelperV2_Removed",
+                schema: "wms.streetname",
+                table: "StreetNameHelperV2",
+                column: "Removed");
+        }
+    }
+}
