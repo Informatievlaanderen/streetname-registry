319a7431547b443d2f97721374d57220af3eaef9
Arne Dumarey <dumarey.arne@gmail.com>
2023-11-24 15:51:10 +0100

    chore: remove crabimport

--- src/StreetNameRegistry.Api.CrabImport/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyDescription("StreetNameRegistry CrabImport API")]
-
-[assembly: ComVisible(false)]
-[assembly: Guid("f8b1f1ad-4653-48ee-ae8e-c8971e66e169")]
