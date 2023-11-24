7c3053cbf33368b2d342f671b6972150dce1cbf2
emalfroy <egon.malfroy@gmail.com>
2023-11-24 14:17:29 +0100

    feat: add StreetNameWasRenamed event

--- src/StreetNameRegistry/Municipality/Events/StreetNameWasRenamed.cs
+++ src/StreetNameRegistry/Municipality/Events/StreetNameWasRenamed.cs
@@ -7,56 +7,48 @@ namespace StreetNameRegistry.Municipality.Events
     using Be.Vlaanderen.Basisregisters.GrAr.Common;
     using Be.Vlaanderen.Basisregisters.GrAr.Provenance;
     using Newtonsoft.Json;
-    using StreetNameRegistry.Municipality;
 
     [EventTags(EventTag.For.Sync, EventTag.For.Edit)]
     [EventName(EventName)]
     [EventDescription("De straatnaam werd hernoemd.")]
-    public sealed class StreetNameWasRenamedV2 : IMunicipalityEvent, IHasPersistentLocalId
+    public sealed class StreetNameWasRenamed : IMunicipalityEvent, IHasPersistentLocalId
     {
         public const string EventName = "StreetNameWasRenamed"; // BE CAREFUL CHANGING THIS!!
 
-        [EventPropertyDescription("Interne GUID van de gemeente aan dewelke de straatnaam is toegewezen.")]
+        [EventPropertyDescription("Interne GUID van de gemeente aan dewelke de straatnaam is gekoppeld.")]
         public Guid MunicipalityId { get; }
-
-        [EventPropertyDescription("NIS-code (= objectidentificator) van de gemeente aan dewelke de straatnaam is toegewezen.")]
-        public string NisCode { get; }
-
-        [EventPropertyDescription("De straatnamen in de officiële en (eventuele) faciliteitentaal van de gemeente. Mogelijkheden: Dutch, French, German of English.")]
-        public IDictionary<Language, string> StreetNameNames { get; }
 
         [EventPropertyDescription("Objectidentificator van de straatnaam.")]
         public int PersistentLocalId { get; }
 
+        [EventPropertyDescription("Objectidentificator van de straatnaam.")]
+        public int DestinationPersistentLocalId { get; }
+
         [EventPropertyDescription("Metadata bij het event.")]
         public ProvenanceData Provenance { get; private set; }
 
-        public StreetNameWasRenamedV2(
+        public StreetNameWasRenamed(
             MunicipalityId municipalityId,
-            NisCode nisCode,
-            Names streetNameNames,
-            PersistentLocalId persistentLocalId)
+            PersistentLocalId persistentLocalId,
+            PersistentLocalId destinationPersistentLocalId)
         {
             MunicipalityId = municipalityId;
-            NisCode = nisCode;
-            StreetNameNames = streetNameNames.ToDictionary();
             PersistentLocalId = persistentLocalId;
+            DestinationPersistentLocalId = destinationPersistentLocalId;
         }
 
         [JsonConstructor]
-        private StreetNameWasRenamedV2(
+        private StreetNameWasRenamed(
             Guid municipalityId,
-            string nisCode,
-            IDictionary<Language, string> streetNameNames,
             int persistentLocalId,
+            int destinationPersistentLocalId,
             ProvenanceData provenance
         ) :
             this(
                 new MunicipalityId(municipalityId),
-                new NisCode(nisCode),
-                new Names(streetNameNames),
-                new PersistentLocalId(persistentLocalId))
-        => SetProvenance(provenance.ToProvenance());
+                new PersistentLocalId(persistentLocalId),
+                new PersistentLocalId(destinationPersistentLocalId))
+            => SetProvenance(provenance.ToProvenance());
 
         public void SetProvenance(Provenance provenance) => Provenance = new ProvenanceData(provenance);
 
@@ -64,9 +56,8 @@ namespace StreetNameRegistry.Municipality.Events
         {
             var fields = Provenance.GetHashFields().ToList();
             fields.Add(MunicipalityId.ToString("D"));
-            fields.Add(NisCode);
             fields.Add(PersistentLocalId.ToString());
-            fields.AddRange(StreetNameNames.Select(streetNameName => $"{streetNameName.Key}: {streetNameName.Value}"));
+            fields.Add(DestinationPersistentLocalId.ToString());
             return fields;
         }
 
