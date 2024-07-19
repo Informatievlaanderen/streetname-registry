a01cb62dfcdee13605b54385f481455945987548
jvandaal <jonas.vandaal@outlook.be>
2024-07-19 09:27:40 +0200

    docs: correct municipality merger events

--- src/StreetNameRegistry/Municipality/Events/StreetNameWasProposedForMunicipalityMerger.cs
+++ src/StreetNameRegistry/Municipality/Events/StreetNameWasProposedForMunicipalityMerger.cs
@@ -11,7 +11,7 @@ namespace StreetNameRegistry.Municipality.Events
 
     [EventTags(EventTag.For.Sync)]
     [EventName(EventName)]
-    [EventDescription("De straatnaam werd voorgesteld voor een gemeente fusie.")]
+    [EventDescription("De straatnaam werd voorgesteld ter voorbereiding van een gemeentefusie.")]
     public sealed class StreetNameWasProposedForMunicipalityMerger : IMunicipalityEvent, IHasPersistentLocalId
     {
         public const string EventName = "StreetNameWasProposedForMunicipalityMerger"; // BE CAREFUL CHANGING THIS!!
