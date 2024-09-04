965a6e6772615b99cc5196108d46bf4c3e76072b
Rik De Peuter <rikdepeuter@gmail.com>
2024-09-04 14:02:05 +0200

    fix: use lifetimescope when dispatching multiple commands for municipality proposal

---
+++ src/StreetNameRegistry.Api.BackOffice.Handlers.Lambda/ScopedIdempotentCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿namespace StreetNameRegistry.Api.BackOffice.Handlers.Lambda
+{
+    using Autofac;
+    using Be.Vlaanderen.Basisregisters.CommandHandling.Idempotency;
+
+    public interface IScopedIdempotentCommandHandler: IIdempotentCommandHandler
+    {
+    }
+
+    public class ScopedIdempotentCommandHandler : IScopedIdempotentCommandHandler
+    {
+        private readonly ILifetimeScope _container;
+
+        public ScopedIdempotentCommandHandler(ILifetimeScope container)
+        {
+            _container = container;
+        }
+
+        public async Task<long> Dispatch(Guid? commandId, object command, IDictionary<string, object> metadata, CancellationToken cancellationToken)
+        {
+            await using var scope = _container.BeginLifetimeScope();
+
+            var resolver = scope.Resolve<IIdempotentCommandHandler>();
+            return await resolver.Dispatch(
+                commandId,
+                command,
+                metadata,
+                cancellationToken);
+        }
+    }
+}
