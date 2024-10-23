02959b5207b8349be892baf3ea5f62d460ccfc47
Arne Dumarey <dumarey.arne@gmail.com>
2024-10-23 09:33:45 +0200

    feat(wfs): add geolocation view

--- src/StreetNameRegistry.Projections.Wfs/Migrations/20241023072631_AddGeolocationView.Designer.cs
+++ src/StreetNameRegistry.Projections.Wfs/Migrations/20241023072631_AddGeolocationView.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StreetNameRegistry.Projections.Wfs;
 
@@ -11,9 +12,11 @@ using StreetNameRegistry.Projections.Wfs;
 namespace StreetNameRegistry.Projections.Wfs.Migrations
 {
     [DbContext(typeof(WfsContext))]
-    partial class WfsContextModelSnapshot : ModelSnapshot
+    [Migration("20241023072631_AddGeolocationView")]
+    partial class AddGeolocationView
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
