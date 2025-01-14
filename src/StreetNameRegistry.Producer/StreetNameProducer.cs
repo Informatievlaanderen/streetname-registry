c3fdb6937d6984dae56fe06bfa48f91642cbd4d2
Arne Dumarey <dumarey.arne@gmail.com>
2025-01-14 15:28:55 +0100

    refactor(producers): convert to consoleapp

---
+++ src/StreetNameRegistry.Producer/StreetNameProducer.cs
@@ -0,0 +1,40 @@
+﻿namespace StreetNameRegistry.Producer
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Be.Vlaanderen.Basisregisters.Projector.ConnectedProjections;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+
+    public sealed class StreetNameProducer : BackgroundService
+    {
+        private readonly IHostApplicationLifetime _hostApplicationLifetime;
+        private readonly ILogger<StreetNameProducer> _logger;
+        private readonly IConnectedProjectionsManager _projectionManager;
+
+        public StreetNameProducer(
+            IHostApplicationLifetime hostApplicationLifetime,
+            IConnectedProjectionsManager projectionManager,
+            ILogger<StreetNameProducer> logger)
+        {
+            _hostApplicationLifetime = hostApplicationLifetime;
+            _projectionManager = projectionManager;
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            try
+            {
+                await _projectionManager.Start(stoppingToken);
+            }
+            catch (Exception exception)
+            {
+                _logger.LogCritical(exception, $"Critical error occured in {nameof(StreetNameProducer)}.");
+                _hostApplicationLifetime.StopApplication();
+                throw;
+            }
+        }
+    }
+}
